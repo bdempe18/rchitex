cca7dfcc16da7a988eb6656efa4d0101d6cadb66
bdempe18 <dempeb18@gmail.com>
2020-01-21 10:38:35 -0500

    switching locals

--- R/build.R
+++ R/build.R
@@ -201,13 +201,14 @@ build.function <- function(...) {
 }
 
 #' print statement for build
-#' @param m Rchitex model to be printed.
+#' @param x Rchitex model to be printed.
+#' @param ... Additional parameters
 #' @export
-print.rchReg <- function(m){
-  if (is.null(m$type))
-    writeLines(m$text, con=stdout())
+print.rchReg <- function(x, ...){
+  if (is.null(x$type))
+    writeLines(x$text, con=stdout())
   else
-    writeLines(m$code, con=stdout())
+    writeLines(x$code, con=stdout())
 }
 
 
